3bf0cf6955c7315df718e1c83dbf299c341650cd
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-01-17 18:28:55 -0600

    added info to page template

--- app.js
+++ app.js
@@ -136,11 +136,18 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+      const pageHTML = generatePage(portfolioData);
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
+    // console.log(portfolioData);
   });
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
